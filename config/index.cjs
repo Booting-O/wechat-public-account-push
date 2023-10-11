722257bfb14fe89d8d97df583cb0b9657790d0dd
MingLiang Yao <54933257+Booting-O@users.noreply.github.com>
2023-10-11 03:47:03 -0500

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oe4pL67MDRWbtfPGB4_2GYIHZ8DM',
+      id: 'oe4pL69zkWotgHTGnN4UsPBMo96Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'LRLVjCWCn77-2TxNRRIVnhPbyMLRn9OgUvoQBh9OeCg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
